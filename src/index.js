c7df45a427a44c29d28f4a6f0fcf9d8a52313f0b
MACHENIKE <462043457@qq.com>
2018-04-20 02:52:28 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import {createStore,applyMiddleware,compose} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import {BrowserRouter,Route,Redirect,Switch} from 'react-router-dom'
+
+import reducers from './reducer'
+import './axiosConfig'
+
+const store = createStore(reducers,compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension?window.devToolsExtension():f=>f
+))
+
+ReactDOM.render((
+  <Provider store={store}>
+    <BrowserRouter>
+    
+    </BrowserRouter>
+  </Provider>
+), document.getElementById('root'));
+registerServiceWorker();
